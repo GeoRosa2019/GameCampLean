6f320effcc674f8665f753782fd23cacd19a996d
GeoRosa2019 <54383138+GeoRosa2019@users.noreply.github.com>
2022-03-30 12:17:02 -0500

    update to enemy and Player

--- Enemy.cs
+++ Enemy.cs
@@ -6,12 +6,37 @@ public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 3f;
+    [SerializeField] private float attackDamage = 10f;
+    [SerializeField] private float attackSpeed = 1f;
+    private float canAttack;
     private Transform target;
 
-    private void Update() {
+    private void FixedUpdate() {
         if (target != null) {
             float step = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            if (attackSpeed <= canAttack){
+            other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+            canAttack = 0f;
+            } else {
+                canAttack += Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            if (attackSpeed <= canAttack){
+            other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
+            canAttack = 0f;
+            } else {
+                canAttack += Time.deltaTime;
+            }
         }
     }
     private void OnTriggerEnter2D(Collider2D other) {
--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     private float health = 0f;
+    [SerializeField] private float maxHealth = 100f;
     // Start is called before the first frame update
     void Start()
     {
+        health = maxHealth;
         
+    }
+
+    public void UpdateHealth(float mod) {
+        health += mod;
+
+        if (health > maxHealth) {
+            health = maxHealth;
+        } else if (health <= 0f) {
+            health = 0f;
+            Debug.Log("Player Respawn");
+        }
     }
 
     // Update is called once per frame
